3b7b4e309ecbdf7c05445301cecd482ea956de61
matthew <matthew.ozon@gmail.com>
2022-08-23 00:01:33 +0300

    loading the proper growth rate

--- data_simulation/growth_and_evap/main.jl
+++ data_simulation/growth_and_evap/main.jl
@@ -54,7 +54,11 @@ load_tim = 3600.0*dropdims(Array{Cdouble}(CSV.File(string(data_folder,"time.csv"
 load_X0  = dropdims(Array{Cdouble}(CSV.File(string(data_folder,"aerodist_initial.csv"); header=false) |> DataFrame),dims=1);
 
 # growth rate #TO CHECK
-load_CGR = Matrix{Cdouble}(CSV.File(string(data_folder,"condensation_rate.csv"); header=false) |> DataFrame)
+# load_CGR = Matrix{Cdouble}(CSV.File(string(data_folder,"condensation_rate.csv"); header=false) |> DataFrame)
+load_cond_length = dropdims(Matrix{Cdouble}(CSV.File(string(data_folder,"cond_area.csv"); header=false) |> DataFrame),dims=1);
+load_diff_vapor  =  CSV.File(string(data_folder,"diff_vapor.csv"); header=false)[1];
+
+GR = load_diff_vapor.Column1[1]*load_cond_length./(load_dia.^2);
 
 # coagulation coefficient
 df_coa = CSV.File(string(data_folder,"coag_kernel.csv"); header=false) |> DataFrame # I have no clue why the last column is interpreted as Strings instead of Float64
@@ -90,10 +94,10 @@ gamma_c = 1.0                          # unit conversion of the wall loss rate
 GR_c = 2.777777777777778e-13           # unit conversion of the condensation growth rate 
 ws = AeroSys(d, gamma_c = gamma_c, GR_c = GR_c);
 ws.is_los = true # false         # linear loss mechanism
-ws.is_coa = true # true         # coagulation mechanism# #WARNING: it can be long
+ws.is_coa = false # true         # coagulation mechanism# #WARNING: it can be long
 ws.is_con = true                 # condensation growth mechanism
 ws.is_nuc = false                 # nucleation mechanism
-ws.is_coa_gain = true # true    # the default is false when the caogulation mechanism is active, you must explicitly activate the coagulation gain if you want both the loss and gain mechanisms to be computed #WARNING: it can be very long in the computation and the initialization of the indices
+ws.is_coa_gain = false # true    # the default is false when the caogulation mechanism is active, you must explicitly activate the coagulation gain if you want both the loss and gain mechanisms to be computed #WARNING: it can be very long in the computation and the initialization of the indices
 if ws.is_coa                     # if the coagulation mechanism is activated, it some more inits are needed (computing the caogulation coefficients and some computation indices to speed up the gain mechanism)
     if true # not computing the coefficient, just plug them in the AeroSys object
         ws.beta[:,:] = 1.0e0load_coa';
@@ -118,30 +122,21 @@ dx_con = zeros(Cdouble,ws.nbin)
 dx_nuc = zeros(Cdouble,ws.nbin)
 dx_los = zeros(Cdouble,ws.nbin)
 GR_t = zeros(Cdouble,ws.nbin,n_samp)
-X[:,1] = load_X0; # ./(1.0e9delta)
+X[:,1] = load_X0.*log10(cst_r); # ./(1.0e9delta)
+load_cond_length
 
 for t in 2:n_samp
     # maximum time step alowed so that the scheme converges without oscillations
     local dt = load_tim[t]-load_tim[t-1];
-    local max_dt = 0.5maximum(abs.(1.0 ./(load_wal + load_CGR[t-1,:]./delta)));
+    local max_dt = 0.5maximum(abs.(1.0 ./(load_wal + 1.0e-9GR./delta)));
     local n_sub_iter = floor(Int64,dt/max_dt)+1; # if dt>dt_max, it is necessary to break the iteration into several sub-iterations with smaller time step
     # time evolution
-    X[:,t] = iter!(dx_coa,dx_con,dx_nuc,dx_los,ws,X[:,t-1],dt/n_sub_iter,load_CGR[t-1,:],0.0,load_wal)
+    X[:,t] = iter!(dx_coa,dx_con,dx_nuc,dx_los,ws,X[:,t-1],dt/n_sub_iter,GR,0.0,load_wal)
     # if dt>max_dt, run the sub iterations
     for _ in 2:n_sub_iter
-        X[:,t] = iter!(dx_coa,dx_con,dx_nuc,dx_los,ws,X[:,t],dt/n_sub_iter,load_CGR[t-1,:],0.0,load_wal)
+        X[:,t] = iter!(dx_coa,dx_con,dx_nuc,dx_los,ws,X[:,t],dt/n_sub_iter,GR,0.0,load_wal)
     end
 end
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -152,14 +147,15 @@ end
 ##
 
 # simulated concentrations
-displayLogData2D(9,load_tim/3600.0,d,X.*(1.0e9delta),max(0.001maximum(X),minimum(X)),maximum(X),_colorbar_label="concentration [\$\\log \\#\$ cm\$^{-3}\$]")
+PSDlog = X./log10(cst_r);
+displayLogData2D(91,load_tim/3600.0,d,PSDlog,max(0.001maximum(PSDlog),minimum(PSDlog)),maximum(PSDlog),_colorbar_label="log distribution \$\\frac{dN}{d\\log_{10}D_p}\$ [\$\\log \\#\$ cm\$^{-3}\$]")
 tight_layout(pad=1.0, w_pad=0.2, h_pad=0.2)
 if SAVE_FIG
     savefig(string(data_folder,"simulated_number_concentration.pdf"))
     savefig(string(data_folder,"simulated_number_concentration.png"))
 end
 if SAVE_DATA
-    CSV.write(string(data_folder,"simulated_data.csv"),DataFrame(X',:auto); writeheader=false)
+    CSV.write(string(data_folder,"simulated_data_psd_log.csv"),DataFrame(PSDlog',:auto); writeheader=false)
 end
 
 
@@ -192,29 +188,29 @@ if SAVE_FIG
 end
 
 # growth rate
-cblabelCond = "condensation rate [nm h\$^{-1}\$]"
-minCond,maxCond = extrema(load_CGR)
-fig,ax,pcm = imshowData(10,load_tim[1:end-1]/3600.0,1.0e9d,load_CGR;_norm=:NoNorm,_vmin=minCond,_vmax=maxCond,_edgecolors="face",_shading="None")
-ax.ticklabel_format(axis="both",style="sci",scilimits=(-1,2),useOffset=true)
-xlabel("time [h]",fontsize=14)
-ylabel("diameter [nm]",fontsize=14)
-yscale("log")
-xticks(fontsize=14); 
-yticks(fontsize=14);
-rc("ytick",color="white")
-cax   = fig.add_axes([0.84, .5, 0.03, 0.4])
-cbar  = fig.colorbar(pcm, orientation="vertical", cax=cax, shrink=0.6)
-cbar.formatter.set_powerlimits((-1,2))
-cbar.update_ticks()
-cbar.set_label("growth rate [nm h\$^{-1}\$]",fontsize=14, color="white")
-cbar.ax.tick_params(labelsize=14)
-cbar.formatter.set_powerlimits((-1,2))
-cbar.ax.yaxis.offsetText.set_size(14)
-cbar.ax.yaxis.set_tick_params(color="white")
-cbar.outline.set_edgecolor("white")
-rc("ytick",color="black")
-tight_layout(pad=1.0, w_pad=0.5, h_pad=0.2)
-if SAVE_FIG
-    savefig(string(data_folder,"growth_rate.pdf"))
-    savefig(string(data_folder,"growth_rate.png"))
-end
+# cblabelCond = "condensation rate [nm h\$^{-1}\$]"
+# minCond,maxCond = extrema(load_CGR)
+# fig,ax,pcm = imshowData(10,load_tim[1:end-1]/3600.0,1.0e9d,load_CGR;_norm=:NoNorm,_vmin=minCond,_vmax=maxCond,_edgecolors="face",_shading="None")
+# ax.ticklabel_format(axis="both",style="sci",scilimits=(-1,2),useOffset=true)
+# xlabel("time [h]",fontsize=14)
+# ylabel("diameter [nm]",fontsize=14)
+# yscale("log")
+# xticks(fontsize=14); 
+# yticks(fontsize=14);
+# rc("ytick",color="white")
+# cax   = fig.add_axes([0.84, .5, 0.03, 0.4])
+# cbar  = fig.colorbar(pcm, orientation="vertical", cax=cax, shrink=0.6)
+# cbar.formatter.set_powerlimits((-1,2))
+# cbar.update_ticks()
+# cbar.set_label("growth rate [nm h\$^{-1}\$]",fontsize=14, color="white")
+# cbar.ax.tick_params(labelsize=14)
+# cbar.formatter.set_powerlimits((-1,2))
+# cbar.ax.yaxis.offsetText.set_size(14)
+# cbar.ax.yaxis.set_tick_params(color="white")
+# cbar.outline.set_edgecolor("white")
+# rc("ytick",color="black")
+# tight_layout(pad=1.0, w_pad=0.5, h_pad=0.2)
+# if SAVE_FIG
+#     savefig(string(data_folder,"growth_rate.pdf"))
+#     savefig(string(data_folder,"growth_rate.png"))
+# end
